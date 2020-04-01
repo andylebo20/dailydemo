6b63f0cf7e09787fa8910c0385f33703fe9a6ecb
Andy Lebowitz <alebo@sbcglobal.net>
2020-04-01 10:49:12 -0400

    initial commit with build folder

---
+++ frontend/build/precache-manifest.9d1f763b743f05511023c8d0e2d460e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ba0e05e5f71e17b10612137938e701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214a8765963ce0cdca08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a847c4a62891a0ac67d",
+    "url": "/static/js/2.3225e708.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/static/js/2.3225e708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214a8765963ce0cdca08",
+    "url": "/static/js/main.ef7c817a.chunk.js"
+  },
+  {
+    "revision": "79002528dab0273055d4",
+    "url": "/static/js/runtime-main.1d2824b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
