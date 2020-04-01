827b6d74d4c1ff44bd1c5a759b9621653ae6e15b
Andy Lebowitz <alebo@sbcglobal.net>
2020-04-01 10:34:51 -0400

    initial commit

--- frontend/src/Room.js
+++ frontend/src/Room.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import firebase from './firebase'
 
 class Room extends React.Component{
 
@@ -7,26 +7,62 @@ class Room extends React.Component{
         super(props)
 
         this.getTheNetworkStats = this.getTheNetworkStats.bind(this)
+        this.saveMetrics = this.saveMetrics.bind(this)
+
+        this.metricsHistory = []
     }
 
     getTheNetworkStats(){
-        this.call.getNetworkStats().then(function(data){
+        if (this.callFrame.meetingState() === "left-meeting"){
+            return
+        }
+        this.callFrame.getNetworkStats().then(function(data){
             console.log(data)
+            setTimeout(this.getTheNetworkStats, 1000)
+            const metricsTemp = data.stats.latest
+            if (metricsTemp.timestamp == null){
+                return
+            }
+            this.metricsHistory.push({
+                videoRecvBitsPerSecond: metricsTemp.videoRecvBitsPerSecond,
+                videoSendBitsPerSecond: metricsTemp.videoSendBitsPerSecond,
+                videoRecvPacketLoss: metricsTemp.videoRecvPacketLoss,
+                videoSendPacketLoss: metricsTemp.videoSendPacketLoss
+            })
+        }.bind(this)).catch(function(error){
+            console.log(error)
+        })
+    }
+
+    saveMetrics(){
+        console.log(this.metricsHistory)
+        this.db.collection("person1").doc().set({ // logs array of metric history for each call that this person creates
+            videoRecvBitsPerSecondARR: this.metricsHistory.map(data => data.videoRecvBitsPerSecond),
+            videoSendBitsPerSecondARR: this.metricsHistory.map(data => data.videoSendBitsPerSecond),
+            videoRecvPacketLossARR: this.metricsHistory.map(data => data.videoRecvPacketLoss),
+            videoSendPacketLossARR: this.metricsHistory.map(data => data.videoSendPacketLoss)
+        }).then(function(){
+            console.log("saved metrics")
         }).catch(function(error){
             console.log(error)
         })
     }
 
     componentDidMount(){
-        var callFrame = window.DailyIframe.createFrame({showLeaveButton: true});
-        callFrame.join({ url: "https://dailydemoapp.daily.co/" + window.location.pathname.split("/")[2]})
-        this.call = callFrame
+        this.db = firebase.firestore()
+        this.callFrame = window.DailyIframe.createFrame({showLeaveButton: true});
+        this.callFrame.join({ url: "https://dailydemoapp.daily.co/" + window.location.pathname.split("/")[2]}).then(function(){
+            setTimeout(this.getTheNetworkStats, 1000)
+        }.bind(this)).catch(function(error){
+            console.log(error)
+        })
     }
 
     render(){
         return(
             <div>
                 <button onClick={this.getTheNetworkStats}>Get Network Stats</button>
+                <button onClick={this.saveMetrics}>Save Metrics to Firebase</button>
             </div>
         )
     }
